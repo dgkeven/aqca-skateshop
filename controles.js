3c4b71491f667d961b6402af4e72b9f697d07983
Keven <dgkeven@outlook.com>
2026-09-01 15:26:46 -0300

    bug fix

--- controles.js
+++ controles.js
@@ -2,6 +2,11 @@ function contate_me() {
     window.location.href = "https://web.whatsapp.com/send?text=Olá,%20tudo%20bem%20?&phone=+5528999581746"
 }
 
-function send_me_notify() {
-    window.setTimeout('alert("Mensagem enviada com sucesso!!!");window.close();', 2000);
+function send_me_notify(event) {
+    // The form has no real backend (action="#"), so stop the page from
+    // reloading and just confirm the message was "sent".
+    if (event) {
+        event.preventDefault();
+    }
+    alert("Mensagem enviada com sucesso!!!");
 }
